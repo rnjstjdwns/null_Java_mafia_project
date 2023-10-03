f437d8bc4813d5c62c6f3eb2bd113941c79ba084
ChaeHaejoong <146797747+ChaeHaejoong@users.noreply.github.com>
2023-10-03 12:59:04 +0900

    Delete java directory

--- java/src/IfSheAintGotAnAPSheIsNotMine/Asdff.java
+++
@@ -1,8 +0,0 @@
-package IfSheAintGotAnAPSheIsNotMine;
-
-public class Asdff {
-	public static void main(String[] args) {
-		Object o = "Hello";
-		System.out.println(o);
-	}
-}
--- java/src/IfSheAintGotAnAPSheIsNotMine/assignRandom.java
+++
@@ -1,5 +0,0 @@
-package IfSheAintGotAnAPSheIsNotMine;
-
-public class assignRandom {
-
-}
