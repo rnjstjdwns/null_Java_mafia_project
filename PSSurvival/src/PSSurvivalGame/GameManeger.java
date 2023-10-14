918aac02f56cc13c12457a31b0daf9ea909f65f2
ChaeHaejoong <146797747+ChaeHaejoong@users.noreply.github.com>
2023-10-15 04:24:20 +0900

    Add files via upload
    플레이어별 이름 선정과 직업 배정 받기까지 코드

---
+++ PSSurvival/src/PSSurvivalGame/GameManeger.java
@@ -0,0 +1,15 @@
+package PSSurvivalGame;
+
+import Player.JobAssignmentManeger;
+
+public class GameManeger {
+	
+	public GameManeger() {
+		new JobAssignmentManeger();
+	}
+	
+
+	public static void main(String[] args) {
+		new GameManeger();
+	}
+}
