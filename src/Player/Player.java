0fbb32b1e3cb8871ac37458988239161fd219a6c
ChaeHaejoong <146797747+ChaeHaejoong@users.noreply.github.com>
2023-10-15 23:49:47 +0900

    Add files via upload
    어레이 리스트에 인티저 값을 넣고 인티저 값에 따라 메소드에서 직업을 배정하는 다운 캐스팅을 진행하는 코드

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -1,31 +1,36 @@
 package Player;
 
-import java.util.ArrayList;
-
-import Item.Item;
-
 public class Player {
 	
 	String name;
-	ArrayList <Item> bag = new ArrayList <> (6);
 	
 	Player() {
 		
 	}
-   
-	public void setName(String name) {
-		this.name = name;
+	
+	
+
+	public void setName() {
+		
 	}
 	
 	public void sayName() {
-		System.out.println("이름을 " + this.name + "(으)로 설정하였습니다.");
+		
 	}
-
+	
+	public void getJob(int i) {
+		if(i == 0) {
+			Professor p = (Professor) this;
+		} else {
+			 Student s = (Student) this;
+		}
+	}
+	
 	public void sayJob(Professor p) {
-		System.out.println("당신의 직업은 교수 입니다.");
+		System.out.println("당신의 직업은 교수입니다.");
 	}
 	
 	public void sayJob(Student s) {
 		System.out.println("당신의 직업은 학생입니다.");
 	}
-}
+ }
