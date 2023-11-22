e28e61d31163213253f26bc7527d30b22880558e
rnjstjdwns <58690800+rnjstjdwns@users.noreply.github.com>
2023-11-23 08:45:19 +0900

    Add files via upload

---
+++ client/chat-client1byn/src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+
+
+import ui.UI;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		
+		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("아이디를 입력하세요 : ");
+//		String id = sc.next();
+		new UI();
+//		new MainClient(id,"122.45.106.119");
+	}
+}
---
+++ client/chat-client1byn/src/ui/GameLobby.java
@@ -0,0 +1,13 @@
+package ui;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class GameLobby extends JFrame{
+	public GameLobby() {
+		// TODO Auto-generated constructor stub
+		
+	}
+}
